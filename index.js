cea91f1ce3050f2eae28bd003b23609db836059e
peerapas.s <peerapas.s@hmcinternal.biz>
2025-08-08 14:37:38 +0700

    main 8868

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
 const express = require("express");
-const xlsx = require('xlsx');
-const path = require('path');
+const xlsx = require("xlsx");
+const path = require("path");
 const app = express();
 const PORT = 8080;
 
-
-
 app.use(express.json());
 
-const {
-  leavebalance,
-  news,
-  quickmenu,
-  managewidget,
-  // lang5555,
-  notifications,
-  leaverequest,
-  overtime,
-  recordtime,
-  shiftexchange,
-  documents,
-  myrequestleave,
-  myrequests,
-  
-} = require("./datajson");
-
+const { quickmenu, managewidget } = require("./datajson");
 
 function readLangExcel() {
-  const workbook = xlsx.readFile(path.join(__dirname, 'translated_lang.xlsx'));
+  const workbook = xlsx.readFile(path.join(__dirname, "translated_lang.xlsx"));
   const sheet = workbook.Sheets[workbook.SheetNames[0]];
   const rows = xlsx.utils.sheet_to_json(sheet);
 
   const lang = {};
-  rows.forEach(row => {
+  rows.forEach((row) => {
     if (row.key && row.th && row.en) {
       lang[row.key] = {
         th: row.th,
@@ -44,7 +26,8 @@ function readLangExcel() {
   return lang;
 }
 
-const FIXED_AUTH = "1c9f8a3c4a3e06a5e7bb6d5b5ed3c8d9c5f2d4eabbc5db6e74c1d12d5fc0ba4c";
+const FIXED_AUTH =
+  "1c9f8a3c4a3e06a5e7bb6d5b5ed3c8d9c5f2d4eabbc5db6e74c1d12d5fc0ba4c";
 
 app.use("/static", express.static(path.join(__dirname, "public")));
 
@@ -66,23 +49,6 @@ app.get("/lang", (req, res) => {
   });
 });
 
-// app.get("/lang5555", (req, res) => {
-//   //   res.status(200).json(quickmenu);
-//   res.status(200).json({
-//     statuscode: 200,
-//     message: "OK",
-//     data: lang5555,
-//   });
-// });
-
-app.get("/leave", (req, res) => {
-  res.status(200).json({
-    statuscode: 200,
-    message: "OK",
-    data: leavebalance,
-  });
-});
-
 app.get("/quickmenu", (req, res) => {
   res.status(200).json({
     statuscode: 200,
@@ -99,78 +65,6 @@ app.get("/managewidget", (req, res) => {
   });
 });
 
-app.get("/news/list", (req, res) => {
-  res.status(200).json({
-    statuscode: 200,
-    message: "OK",
-    data: news,
-  });
-});
-
-app.get("/notifications", (req, res) => {
-  res.status(200).json({
-    statuscode: 200,
-    message: "OK",
-    data: notifications,
-  });
-});
-
-app.get("/leaverequest", (req, res) => {
-  res.status(200).json({
-    statuscode: 200,
-    message: "OK",
-    data: leaverequest,
-  });
-});
-
-app.get("/overtime", (req, res) => {
-  res.status(200).json({
-    statuscode: 200,
-    message: "OK",
-    data: overtime,
-  });
-});
-
-app.get("/recordtime", (req, res) => {
-  res.status(200).json({
-    statuscode: 200,
-    message: "OK",
-    data: recordtime,
-  });
-});
-
-app.get("/shiftexchange", (req, res) => {
-  res.status(200).json({
-    statuscode: 200,
-    message: "OK",
-    data: shiftexchange,
-  });
-});
-
-app.get("/documents", (req, res) => {
-  res.status(200).json({
-    statuscode: 200,
-    message: "OK",
-    data: documents,
-  });
-});
-
-app.get("/myrequestleave", (req, res) => {
-  res.status(200).json({
-    statuscode: 200,
-    message: "OK",
-    data: myrequestleave,
-  });
-});
-
-app.get("/myrequests", (req, res) => {
-  res.status(200).json({
-    statuscode: 200,
-    message: "OK",
-    data: myrequests,
-  });
-});
-
 app.get("/statuscheckin", (req, res) => {
   res.status(200).json({
     statuscode: 200,
@@ -179,55 +73,37 @@ app.get("/statuscheckin", (req, res) => {
   });
 });
 
-
-
 app.get("/baseversion", (req, res) => {
   res.status(200).json({
     statuscode: 200,
     message: "OK",
-    data: '1',
+    data: "1",
   });
 });
 
-
-
 app.get("/banners", (req, res) => {
-  const protocol = req.protocol; // http หรือ https
-  const host = req.headers.host; // localhost:8080 หรือ ip:8080
+  const protocol = req.protocol;
+  const host = req.headers.host;
 
   const baseUrl = `${protocol}://${host}`;
 
   const banners = [
     {
       title: "Tigersoft 1998",
-      image: `${baseUrl}/static/app-banner-tiger-HR-Openspace-04.webp`,
-      url: `https://tigersoft.co.th/banner-app/`,
+      image: `${baseUrl}/static/Banner App-01.png`,
+      url: `https://tigersoft.co.th/`,
     },
     {
       title: "Tigersoft 1998",
-      image: `${baseUrl}/static/Banner-ERP-app-01-1.webp`,
-      url: `https://tigersoft.co.th/banner-app/`,
+      image: `${baseUrl}/static/Banner App-02.png`,
+      url: `https://tigersoft.co.th/blog-articles/`,
     },
     {
       title: "Tigersoft 1998",
-      image: `${baseUrl}/static/banner-KMS-app.webp`,
-      url: `https://tigersoft.co.th/banner-app/`,
+      image: `${baseUrl}/static/Banner App-03.png`,
+      url: `https://www.facebook.com/Tigersoft.co.th/`,
     },
-    {
-      title: "Tigersoft 1998",
-      image: `${baseUrl}/static/CoverApp-Customer-Service-Overtime.webp`,
-      url: `https://tigersoft.co.th/banner-app/`,
-    },
-    {
-      title: "Tigersoft 1998",
-      image: `${baseUrl}/static/haas-webapp-banner.webp`,
-      url: `https://tigersoft.co.th/banner-app/`,
-    },
-    {
-      title: "Tigersoft 1998",
-      image: `${baseUrl}/static/Web-Tigersoft-HR-Software-Banner-Conicle-crop-1.webp`,
-      url: `https://tigersoft.co.th/banner-app/`,
-    },
+   
   ];
 
   res.status(200).json({
